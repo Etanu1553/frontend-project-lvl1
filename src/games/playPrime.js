524b319530629634c39e96a39cdbf923f4572da5
Sofiia Mironova <etanu1553@gmail.com>
2022-02-11 21:07:18 +0300

    correct code climate playPrime

--- src/games/playPrime.js
+++ src/games/playPrime.js
@@ -3,21 +3,23 @@ import {
   userName, getRandomInt, roundGame, checkAnswer,
 } from '../index.js';
 
+export function isPrime(num) {
+  for (let n = 2; n < num; n += 1) {
+    if (num % n === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+}
+
 export default function playPrime() {
   let count = 0;
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
   for (let i = 0; i < roundGame; i += 1) {
     const num = getRandomInt(2, 100);
     console.log(`Question: ${num}`);
-    let numPrime = 'yes';
     const textAnswer = readlineSync.question('Your answer: ');
-    for (let n = 2; n < num; n += 1) {
-      if (num % n === 0) {
-        numPrime = 'no';
-        break;
-      }
-    }
-    if (checkAnswer(textAnswer, numPrime)) {
+    if (checkAnswer(textAnswer, isPrime(num))) {
       count += 1;
     } else {
       break;
