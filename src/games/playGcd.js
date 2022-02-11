7ad925b5390c486463a897c6906cf8eb93f9d278
Sofiia Mironova <etanu1553@gmail.com>
2022-02-11 21:02:30 +0300

    correct code climate playGcd

--- src/games/playGcd.js
+++ src/games/playGcd.js
@@ -3,22 +3,29 @@ import {
   userName, getRandomInt, roundGame, checkAnswer,
 } from '../index.js';
 
+export function gcd(x, y) {
+  let a = x;
+  let b = y;
+  while (a !== b) {
+    if (a > b) {
+      a -= b;
+    } else {
+      b -= a;
+    }
+  }
+  return a;
+}
+
+// console.log(gcd(24, 18))
+
 export default function playGcd() {
   let count = 0;
   console.log('Find the greatest common divisor of given numbers.');
   for (let i = 0; i < roundGame; i += 1) {
-    let x = getRandomInt(1, 100);
-    let y = getRandomInt(1, 100);
+    const x = getRandomInt(1, 100);
+    const y = getRandomInt(1, 100);
     console.log(`Question: ${x} ${y}`);
-    let divisorNum = x;
-    while (x !== y) {
-      if (x > y) {
-        x %= y;
-      } else {
-        y %= x;
-      }
-      divisorNum = x;
-    }
+    const divisorNum = gcd(x, y);
     const textAnswer = Number(readlineSync.question('Your answer: '));
     if (checkAnswer(textAnswer, divisorNum)) {
       count += 1;
